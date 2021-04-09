6b76cf75b9036a123a6ae35595d2361bfd61891b
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-04-09 14:35:14 +0200

    exercise 2 almost done. working on View

--- src/main/java/se/lexicon/springmvcexercise/controllers/Home.java
+++ src/main/java/se/lexicon/springmvcexercise/controllers/Home.java
@@ -3,7 +3,6 @@ package se.lexicon.springmvcexercise.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,19 +18,23 @@ public class Home {
     public String index(){
         return "index";
     }
-    @GetMapping("/contactform")
-    public String contactForm() {
+
+    @GetMapping("/contactForm")
+    public String contactForm(Model model) {
+        String input = "";
+        model.addAttribute("input", input);
         return "contact";
     }
 
     @PostMapping("/contact")
-    public String contactInfo(@RequestParam String contactInfo){
+    public String contactInfo(@RequestParam(name = "contactInfo") String contactInfo){
+        System.out.println("contactInfo = " + contactInfo);
         stringList.add(contactInfo);
         return "contact";
     }
     @GetMapping("/contact")
     public String contactList(Model model) {
         model.addAttribute("stringList",stringList);
-                return "redirect: /home/";
+                return "contact";
     }
 }
